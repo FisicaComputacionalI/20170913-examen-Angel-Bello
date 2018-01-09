faa8c8fc11c29a13b263bdfac12aeeecac9f6152
Isabel Pedraza <isapedraza@hotmail.com>
2018-01-09 01:00:48 +0100

    calificacion de examen

--- E2.py
+++ E2.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 ax = plt.subplot(111)
 t = np.arange(0.0,5, 0.01)
+#La magnitud de la funci'on coseno multiplica al coseno. Si entraste en el 2014, al menos llevas 3 anios. La constante se suma a la funci'on no al argumento. 
+#La definici'on m'as apropiada ser'ia 3*np.cos(2*np.pi*t)+2014
 s = np.cos(2*np.pi*t+2014)
 line, = plt.plot(t, 3*s, lw=1)
 plt.ylim(-5,5)
